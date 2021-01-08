b4b95ca20f5b3dfcc7730e3d031e2782cbb5efed
Filip Martinsson <martinsson.filip@gmail.com>
2021-01-08 14:48:18 +0100

    added new 201 code, debugging section

---
+++ Debugging/1.sol
@@ -0,0 +1,55 @@
+pragma solidity 0.7.5;
+pragma abicoder v2;
+import "./Ownable.sol";
+
+interface GovermentInterface{
+    function addTransaction(address _from, address _to, uint _amount) external payable;
+}
+
+contract Bank is Ownable {
+    
+    GovermentInterface govermentInstance = GovermentInterface(0x3328358128832A260C76A4141e19E2A943CD4B6D);
+    
+    mapping(address => uint) balance;
+    
+    event depositDone(uint amount, address indexed depositedTo);
+    
+    
+    function deposit() public payable returns (uint)  {
+        balance[msg.sender] = msg.value;
+        emit depositDone(msg.value, msg.sender);
+        return balance[msg.sender];
+    }
+    
+    function withdraw(uint amount) public onlyOwner returns (uint){
+        require(balance[msg.sender] >= amount);
+        msg.sender.transfer(amount);
+        return balance[msg.sender];
+    }
+    
+    function getBalance() public view returns (uint){
+        return balance[msg.sender];
+    }
+    
+    function transfer(address recipient, uint amount) public {
+        require(balance[msg.sender] >= amount, "Balance not sufficient");
+        require(msg.sender != recipient, "Don't transfer money to yourself");
+        
+        uint previousSenderBalance = balance[msg.sender];
+        
+        _transfer(msg.sender, recipient, amount);
+        
+        //govermentInstance.addTransaction(msg.sender, recipient, amount);
+        
+        assert(balance[msg.sender] == previousSenderBalance - amount);
+    }
+    
+    function _transfer(address from, address to, uint amount) private {
+        balance[from] - amount;
+        balance[to] + amount;
+    }
+      function getTransfers() external view returns(uint[] memory) {
+
+    }
+    
+}
