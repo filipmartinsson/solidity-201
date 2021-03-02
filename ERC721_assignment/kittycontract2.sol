d29f947abae09d259ebcc5b45d3a7f657fb87a05
Filip Martinsson <martinsson.filip@gmail.com>
2021-03-02 13:42:32 +0100

    updated

---
+++ ERC721_assignment/kittycontract2.sol
@@ -0,0 +1,123 @@
+pragma solidity ^0.8.0;
+
+contract Kittycontract {
+
+    string public constant name = "TestKitties";
+    string public constant symbol = "TK";
+    
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    event Birth(
+        address owner,
+        uint256 kittenId, 
+        uint256 mumId, 
+        uint256 dadId, 
+        uint256 genes
+    );
+
+    struct Kitty {
+        uint256 genes;
+        uint64 birthTime;
+        uint32 mumId;
+        uint32 dadId;
+        uint16 generation;
+    }
+    
+    Kitty[] kitties;
+
+    mapping (uint256 => address) public kittyIndexToOwner;
+    
+    mapping (address => uint256) ownershipTokenCount;
+    
+    mapping (address => uint256[]) ownerToCats;
+
+
+    
+
+    function balanceOf(address owner) external view returns (uint256 balance){
+        return ownershipTokenCount[owner];
+    }
+
+    function totalSupply() public view returns (uint) {
+        return kitties.length;
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address)
+    {
+        return kittyIndexToOwner[_tokenId];
+    }
+
+    function transfer(address _to, uint256 _tokenId) external
+    {
+        require(_to != address(0));
+        require(_to != address(this));
+        require(_owns(msg.sender, _tokenId));
+
+        _transfer(msg.sender, _to, _tokenId);
+    }
+    
+    function getAllCatsFor(address _owner) external view returns (uint[] memory cats){
+        return ownerToCats[_owner];
+    }
+    
+    function createKittyGen0(uint256 _genes) public returns (uint256) {
+        return _createKitty(0, 0, 0, _genes, msg.sender);
+    }
+
+    function _createKitty(
+        uint256 _mumId,
+        uint256 _dadId,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) private returns (uint256) {
+        Kitty memory _kitty = Kitty({
+            genes: _genes,
+            birthTime: uint64(block.timestamp),
+            mumId: uint32(_mumId),
+            dadId: uint32(_dadId),
+            generation: uint16(_generation)
+        });
+        
+        kitties.push(_kitty);
+
+        uint256 newKittenId = kitties.length - 1;
+
+        emit Birth(_owner, newKittenId, _mumId, _dadId, _genes);
+
+        _transfer(address(0), _owner, newKittenId);
+
+        return newKittenId;
+
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) internal {
+        ownershipTokenCount[_to]++;
+
+        kittyIndexToOwner[_tokenId] = _to;
+        ownerToCats[_to].push(_tokenId);
+
+        if (_from != address(0)) {
+            ownershipTokenCount[_from]--;
+            _removeTokenIdFromOwner(_from, _tokenId);
+        }
+
+        // Emit the transfer event.
+        emit Transfer(_from, _to, _tokenId);
+    }
+    
+    function _removeTokenIdFromOwner(address _owner, uint256 _tokenId) internal {
+        uint256 lastId = ownerToCats[_owner][ownerToCats[_owner].length-1];
+        for (uint i = 0; i < ownerToCats[_owner].length; i++) {
+            if (ownerToCats[_owner][i] == _tokenId) {
+                ownerToCats[_owner][i] = lastId;
+                ownerToCats[_owner].pop();
+            }
+        }
+    }
+
+    function _owns(address _claimant, uint256 _tokenId) internal view returns (bool) {
+        return kittyIndexToOwner[_tokenId] == _claimant;
+    }
+
+}
---
+++ openzeppelin-token-assignment-solution.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+
+/**
+ * @dev {ERC20} token, including:
+ *
+ *  - ability for holders to burn (destroy) their tokens
+ *  - a minter role that allows for token minting (creation)
+ *  - a pauser role that allows to stop all token transfers
+ *
+ * This contract uses {AccessControl} to lock permissioned functions using the
+ * different roles - head to its documentation for details.
+ *
+ * The account that deploys the contract will be granted the minter and pauser
+ * roles, as well as the default admin role, which will let it grant both minter
+ * and pauser roles to other accounts.
+ */
+contract MyToken is Context, AccessControl, ERC20Burnable, ERC20Pausable {
+    using SafeMath for uint256;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant CAP_ROLE = keccak256("CAP_ROLE");
+
+    uint256 private _cap;
+
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+     * account that deploys the contract.
+     *
+     * See {ERC20-constructor}.
+     */
+    constructor(string memory name, string memory symbol, uint256 cap_) public ERC20(name, symbol) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+        _setupRole(CAP_ROLE, _msgSender());
+
+        //Set Cap
+        require(cap_ > 0, "ERC20Capped: cap is 0");
+        _cap = cap_;
+    }
+
+    /**
+     * @dev Returns the cap on the token's total supply.
+     */
+    function cap() public view virtual returns (uint256) {
+        return _cap;
+    }
+
+    /**
+     * @dev Changes the cap on the token's total supply.
+     */
+    function setCap(uint256 cap_) public virtual {
+        require(hasRole(CAP_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have cap role to set cap");
+        require(cap_ > 0, "ERC20Capped: cap is 0");
+        _cap = cap_;
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have minter role to mint");
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Pauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_pause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have pauser role to pause");
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_unpause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have pauser role to unpause");
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+
+        if (from == address(0)) { // When minting tokens
+            require(totalSupply().add(amount) <= cap(), "ERC20Capped: cap exceeded");
+        }
+    }
+}
